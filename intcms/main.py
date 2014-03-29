b238c0c2e2fc15ed2eb52379238ad07e7dfb8bda
Sean Zhu <interestinglythere@gmail.com>
2014-03-29 14:03:08 -0700

    add get_http_info to main

--- intcms/main.py
+++ intcms/main.py
@@ -3,8 +3,8 @@
 def main():
 	from intcms import parse_environment
 	parse_environment()
-	from intcms import Environment, PAGE, TEMPLATES
-	env = Environment.create_jinja_env(PAGE, TEMPLATES)
+	from intcms import Environment, PAGE, TEMPLATES, get_http_info
+	env = Environment.create_jinja_env(PAGE, TEMPLATES, get_http_info())
 
 	import extensions
 
